77ab722a6a1f6b23c9c7d031a2c20ef527b229f8
ollekil <ollekil@kakao.com>
2024-07-26 05:25:08 +0900

    Step 3 : 로또 당첨 번호를 회차별로 저장하고 확인하는 함수 호출

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -14,3 +14,13 @@ import Foundation
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 // 찍은 번호와 로또 당첨 번호 의 겹치는 숫자를 확인하는 함수 호출
 playLottoGame(myLottoNumbers: myLottoNumbers)
+
+
+// MARK: - Step 3: 로또 당첨번호를 회차별로 저장하고, 확인하자!
+
+// 로또 번호 5번 생성하고 저장
+generateAndSaveLottoNumbers()
+
+// 특정 회차의 로또 번호 확인 1~5
+printSpecificLottoNumbers(round: 3)
+
