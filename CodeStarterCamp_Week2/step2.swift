821844affc25393cd43c95c7ca7ae8fc2d810209
Elin <byrr113@gmail.com>
2024-08-19 21:28:17 +0900

    로또 당첨 번호 생성 함수 추가

--- CodeStarterCamp_Week2/step2.swift
+++ CodeStarterCamp_Week2/step2.swift
@@ -8,12 +8,27 @@
 import Foundation
 
 let myLottoNumbers: Set<Int> = [11, 26, 3, 4, 35, 6]
+var winningLottoNumbers: Set<Int> = []
 
-let winningLottoNumbers: Set<Int> = [3, 19, 8, 15, 4, 31]
+func makeWinningNumbers(lottery: [Int]) {
+    if lottery.count == 6 {
+        for number in 0...5 {
+            winningLottoNumbers.insert(lottery[number])
+        }
+        if winningLottoNumbers.count < 6 {
+            print("중복 값이 있습니다.")
+        }
+    } else if lottery.count < 6 {
+        print("당첨 번호는 6개 입니다. 번호를 더 입력하세요.")
+    } else {
+        print("당첨 번호는 6개 입니다. 번호를 삭제하세요.")
+    }
+}
 
 let result: Set<Int> = winningLottoNumbers.intersection(myLottoNumbers)
 
-func matchLottoNumbers() {
+func matchLottoNumbers(lottery: [Int]) {
+    makeWinningNumbers(lottery: lottery)
     if result.count > 0 {
         print("축하합니다! 겹치는 번호는 \(result.sorted())입니다!")
     } else {
---
+++ CodeStarterCamp_Week2/step3.swift
@@ -0,0 +1,8 @@
+//
+//  step3.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by 변예린 on 8/19/24.
+//
+
+import Foundation
