e0ad7f2956b4e687817a19c6742ff89634a1828c
Elin <byrr113@gmail.com>
2024-08-20 20:25:13 +0900

    당첨번호 생성 함수 while문으로 수정, 중복값 입력 조건 제거

--- CodeStarterCamp_Week2/step2.swift
+++ CodeStarterCamp_Week2/step2.swift
@@ -11,29 +11,21 @@ let myLottoNumbers: Set<Int> = [11, 26, 3, 4, 35, 6]
 var winningLottoNumbers: Set<Int> = []
 
 func makeWinningNumbers(lottery: [Int]) {
-    if lottery.count == 6 {
-        for number in 0...5 {
-            winningLottoNumbers.insert(lottery[number])
-        }
-        if winningLottoNumbers.count < 6 {
-            print("중복 값이 있습니다. 서로 다른 6개의 번호를 입력하세요.")
-        }
-    } else if lottery.count < 6 {
-        print("당첨 번호는 6개 입니다. 번호를 더 입력하세요.")
-    } else {
-        print("당첨 번호는 6개 입니다. 번호를 삭제하세요.")
+    var number = 0
+    while winningLottoNumbers.count < 6 {
+        winningLottoNumbers.insert(lottery[number])
+        number += 1
     }
 }
+
 
 let result: Set<Int> = winningLottoNumbers.intersection(myLottoNumbers)
 
 func matchLottoNumbers(lottery: [Int]) {
     makeWinningNumbers(lottery: lottery)
-    if winningLottoNumbers.count == 6 {
-        if result.count > 0 {
-            print("축하합니다! 겹치는 번호는 \(result.sorted())입니다!")
-        } else {
-            print("아쉽지만 겹치는 번호가 없습니다.")
-        }
+    if result.count > 0 {
+        print("축하합니다! 겹치는 번호는 \(result.sorted())입니다!")
+    } else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
     }
 }
